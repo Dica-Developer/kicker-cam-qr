2f7a648c9ac86a60b3e9ba7d90cb285f2e523377
Jörg Weber <weber.joerg.h@gmail.com>
2014-12-24 15:49:40 +0100

    * add test

--- qr.spec.js
+++ qr.spec.js
@@ -2,15 +2,16 @@
 
 var should = require('should');
 var _ = require('lodash');
-var qr = require('./qr');
+var Reader = require('./qr');
 
 describe('QR reader', function () {
 
   it('should find all 4 player', function (done) {
-    this.timeout(30000);
-    var player = {};
+    this.timeout(10000);
+    var player = {},
+      qr = new Reader();
 
-    qr.cb = function (playerName, x, y, playerColor, playerPosition) {
+    var callback = function (playerName, x, y, playerColor, playerPosition) {
       player[playerName] = {
         color: playerColor,
         position: playerPosition
@@ -22,6 +23,40 @@ describe('QR reader', function () {
       }
     };
 
-    qr.loadImage(__dirname + '/test-assets/qr-code-test.png');
+    qr.loadImage(__dirname + '/test-assets/qr-code-test.png', callback);
+  });
+
+  it('should find player with correct position and color', function (done) {
+    this.timeout(10000);
+    var player = {},
+      qr = new Reader();
+
+    var callback = function (playerName, x, y, playerColor, playerPosition) {
+      player[playerName] = {
+        color: playerColor,
+        position: playerPosition
+      };
+
+      if (_.size(player) === 4) {
+        var player1 = player['TwerkJörg'],
+          player2 = player['mascha'],
+          player3 = player['3Dfx'],
+          player4 = player['Slava'];
+
+        player1.color.should.eql('red');
+        player2.color.should.eql('blue');
+        player3.color.should.eql('blue');
+        player4.color.should.eql('red');
+
+
+        player1.position.should.eql('offense');
+        player2.position.should.eql('offense');
+        player3.position.should.eql('defense');
+        player4.position.should.eql('defense');
+        done();
+      }
+    };
+
+    qr.loadImage(__dirname + '/test-assets/qr-code-test.png', callback);
   });
 });
